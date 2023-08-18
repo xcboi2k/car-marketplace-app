fdeb1a75c4da429722d80889f95df9b4ceeb51de
xcboi2k <darkspikyshark@gmail.com>
2023-08-18 16:37:49 +0800

    feat: initial formik in signup

--- components/screens/SignUpScreen/SignUpScreen.js
+++ components/screens/SignUpScreen/SignUpScreen.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useFormik } from "formik";
+import uuid from "react-native-uuid";
 
 import { ButtonContainer, HeaderHolder, HeaderText, HolderContainer, Logo, LogoHolder, SignInText, SignUpContainer, SubText } from './styles'
 
@@ -7,6 +9,38 @@ import ButtonText from '../../shared/ButtonText/ButtonText'
 import TextInput from '../../shared/TextInput/TextInput'
 
 const SignUpScreen = () => {
+    const initialValues = {
+        userName: "",
+        email: "",
+        password: "",
+    };
+
+    const handleFormikSubmit = async (values, { resetForm }) => {
+        console.log(values);
+        if (values.firstName === "" || values.lastName === "" || values.email === "" || values.password === "") {
+            Alert.alert("Incomplete Input", "Please fill up your first name, last name, email and password");
+        } else {
+            // let imgFile;
+            // if (image) {
+            //     imgFile = await uploadImage();
+            // }
+            // addUser({
+            //     firstName: values.firstName,
+            //     lastName: values.lastName,
+            //     email: values.email,
+            //     password: values.password,
+            //     profile_img_ref: imgFile ? imgFile.imgRef : "",
+            //     profile_img: imgFile ? imgFile.imgUri : "",
+            // });
+            resetForm();
+        }
+    };
+
+    const formik = useFormik({
+        initialValues: initialValues,
+        onSubmit: handleFormikSubmit,
+    });
+
     return (
         <SignUpContainer>
             <LogoHolder>
@@ -19,8 +53,8 @@ const SignUpScreen = () => {
                 <TextInput 
                     inputProps={{
                         placeholder: "Enter Username",
-                        // onChangeText: formik.handleChange("wishlistName"),
-                        // value: formik.values.wishlistName,
+                        onChangeText: formik.handleChange("userName"),
+                        value: formik.values.userName,
                     }}
                     customLabel="Username:"
                     labelTextSize = '16px'
@@ -28,8 +62,8 @@ const SignUpScreen = () => {
                 <TextInput 
                     inputProps={{
                         placeholder: "Enter Email Address",
-                        // onChangeText: formik.handleChange("wishlistName"),
-                        // value: formik.values.wishlistName,
+                        onChangeText: formik.handleChange("email"),
+                        value: formik.values.email,
                     }}
                     customLabel="Email Address:"
                     labelTextSize = '16px'
@@ -37,8 +71,8 @@ const SignUpScreen = () => {
                 <TextInput 
                     inputProps={{
                         placeholder: "Enter Password",
-                        // onChangeText: formik.handleChange("wishlistName"),
-                        // value: formik.values.wishlistName,
+                        onChangeText: formik.handleChange("password"),
+                        value: formik.values.password,
                     }}
                     customLabel="Password:"
                     labelTextSize = '16px'
